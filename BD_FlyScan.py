9e9b4014c08c1ab31bad6c814d954a701e315c7a
Benoit Dehapiot <b.dehapiot@gmail.com>
2021-02-18 17:00:42 +0100

    Update BD_FlyScan.py

--- BD_FlyScan.py
+++ BD_FlyScan.py
@@ -9,29 +9,29 @@ from skimage.util import invert
 from skimage.measure import regionprops
 from skimage.filters.rank import gradient
 from skimage.draw import rectangle_perimeter
-from skimage.filters import threshold_triangle
+from skimage.segmentation import clear_border
 from skimage.morphology import disk, dilation, erosion, remove_small_objects, label
 
 #%% Inputs
-RootPath = 'C:/Datas/CurrentData/CENTURIProject_IBDM_MatthieuCavey/21-02-05_TestMovies'
-Filename = '/DUP_21-02-05_Pi01_BrightCorrected.tif'
-ROIMaskname = '/Pi01_ROIMask.tif'
+RootPath = 'D:/CurrentTasks/CENTURIProject_IBDM_MatthieuCavey/21-02-05_TestMovies'
+Filename = '/21-02-05_Pi04_DUP.tif'
+ROIMaskname = '/Pi04_ROIMask.tif'
 
 # General options
-threshCoeff = 0.5 # adjust auto thresholding (the smaller the more sensitive segmentation)
+threshCoeff = 5 # adjust auto thresholding (the smaller the more sensitive segmentation)
 
 # Display options
-ShowBinaryOutlines = 0;
-ShowROIOutlines = 0;
+ShowBinaryOutlines = 0
+ShowROIOutlines = 1
 
 # Advanced options
-BinMinSize1 = 250 # remove binary objects smaller than BinMinSize1
+BinMinSize1 = 150 # remove binary objects smaller than BinMinSize1
 
 #%% Open stack
 
 # Open Stack from filename
 Stack = io.imread(RootPath+Filename)
-ROIMask = io.imread(RootPath+ROIMaskname)
+ROIMask = io.imread(RootPath+ROIMaskname)/255
 nT = Stack.shape[0] # Get Stack dimension (t)
 nY = Stack.shape[1] # Get Stack dimension (x)
 nX = Stack.shape[2] # Get Stack dimension (y)
@@ -44,17 +44,18 @@ BGSub = invert(BGSub)
 
 # with napari.gui_qt():
 #     viewer = napari.view_image(BGSub)
-
+    
 #%% Get BinaryMask (skimage) 
 
-thresh = threshold_triangle(BGSub)
+thresh = np.mean(BGSub)
 def FlyScanBinaryMask(Input):
     # Thresholding
     BinaryMask = Input > thresh*threshCoeff
     # Remove small objects
     BinaryMask = np.array(BinaryMask, dtype=bool) # Convert to boolean
-    BinaryMask = remove_small_objects(BinaryMask, min_size=BinMinSize1)
-    BinaryMask = dilation(BinaryMask)
+    BinaryMask = remove_small_objects(BinaryMask, min_size=BinMinSize1) # Remove small objects
+    BinaryMask = dilation(BinaryMask) # Dilate Binary Mask
+    BinaryMask = clear_border(BinaryMask) # Remove objects connected to the border
     BinaryMask = np.array(BinaryMask, dtype=float) # Convert back to float
     
     # Label objects
@@ -80,8 +81,8 @@ BinaryMask = np.stack(SubList1, axis=0)
 Labels = np.stack(SubList2, axis=0)
 Gradient = np.stack(SubList3, axis=0)
 
-with napari.gui_qt():
-    viewer = napari.view_image(BinaryMask)
+# with napari.gui_qt():
+#     viewer = napari.view_image(BinaryMask)
 
 #%% Define Regions of interest
 
@@ -94,12 +95,16 @@ for i in range(len(Unique)-1):
         ROIMaskLabel[ROIMaskLabel == tempProps[i].label] = 2
     else:
         ROIMaskLabel[ROIMaskLabel == tempProps[i].label] = 1
-        
+                
 # Get up and down horizontal limits
 YLimits = np.mean(ROIMask,axis=1)
 for i in range(nY):
     if YLimits[i] == 0:
         ROIMaskLabel[i,:] = 2
+        
+# Define ROIMaskValid        
+ROIMaskValid = np.ones([nY,nX])
+ROIMaskValid[ROIMaskLabel==2] = 0
 
 #%% Get objects properties
 
@@ -108,20 +113,23 @@ for i in range(nT):
     Unique = np.unique(Labels[i,:,:])
     rProps = regionprops(Labels[i,:,:])
     for j in range(len(Unique)-1):       
-        if ROIMaskLabel[round(rProps[j].centroid[0]),round(rProps[j].centroid[1])] < 2:
-            tempProps = pd.DataFrame(index=np.arange(0),columns=['timepoint', 'label', 'area', 'ctrd_X','ctrd_Y', 'grd_SD', 'grd_Quant'])            
+            tempProps = pd.DataFrame(index=np.arange(0),columns=['timepoint', 'label', 'area', 'ctrd_X','ctrd_Y', 'grd_SD', 'grd_Quant', 'class'])            
             tempCoords = rProps[j].coords
+            tempValROIMaskValid  = np.zeros([len(tempCoords),1])
             tempValGradient = np.zeros([len(tempCoords),1])
             for k in range(len(tempCoords)-1):
-                tempValGradient[k] = Gradient[i,tempCoords[k,0],tempCoords[k,1]]               
-            tempProps.at[j,'timepoint'] = i
-            tempProps.at[j,'label'] = rProps[j].label
-            tempProps.at[j,'area'] = rProps[j].area
-            tempProps.at[j,'ctrd_X'] = rProps[j].centroid[1]
-            tempProps.at[j,'ctrd_Y'] = rProps[j].centroid[0]       
-            tempProps.at[j,'grd_SD'] = np.std(tempValGradient)
-            tempProps.at[j,'grd_Quant'] = np.quantile(tempValGradient,0.9)
-            MergedProps = pd.concat([MergedProps, tempProps],ignore_index=True)     
+                tempValROIMaskValid[k] = ROIMaskValid[tempCoords[k,0],tempCoords[k,1]]  
+                tempValGradient[k] = Gradient[i,tempCoords[k,0],tempCoords[k,1]]                
+            if np.mean(tempValROIMaskValid) >= 0.90:          
+                tempProps.at[j,'timepoint'] = i
+                tempProps.at[j,'label'] = rProps[j].label
+                tempProps.at[j,'area'] = rProps[j].area
+                tempProps.at[j,'ctrd_X'] = rProps[j].centroid[1]
+                tempProps.at[j,'ctrd_Y'] = rProps[j].centroid[0]       
+                tempProps.at[j,'grd_SD'] = np.std(tempValGradient)
+                tempProps.at[j,'grd_Quant'] = np.quantile(tempValGradient,0.9)
+                tempProps.at[j,'class'] = np.nan
+                MergedProps = pd.concat([MergedProps, tempProps],ignore_index=True)     
     
 # Normalize data
 MergedProps['area'] = MergedProps['area']/np.mean(MergedProps['area'])
@@ -155,20 +163,28 @@ for i in range(len(MergedProps)):
 BinaryOutlines = np.zeros([nT,nY,nX])
 for i in range(nT):
     BinaryOutlines[i,:,:,] = np.subtract(BinaryMask[i,:,:,],erosion(BinaryMask[i,:,:,],disk(1)))
-ROIMaskOutlines = np.subtract(ROIMask,erosion(ROIMask,disk(1)))
+ROIOutlines = np.subtract(ROIMask,erosion(ROIMask,disk(1)))
 
-
-tempDisplay = np.maximum(TrackedObjects,BinaryOutlines)
+if ShowBinaryOutlines == 1 and ShowROIOutlines == 1:
+    tempDisplay = np.maximum(TrackedObjects,BinaryOutlines)
+    tempDisplay = np.maximum(tempDisplay,ROIOutlines)
+elif ShowBinaryOutlines == 1 and ShowROIOutlines == 0:
+    tempDisplay = np.maximum(TrackedObjects,BinaryOutlines)
+elif ShowBinaryOutlines == 0 and ShowROIOutlines == 1: 
+    tempDisplay = np.maximum(TrackedObjects,ROIOutlines)
+elif ShowBinaryOutlines == 0 and ShowROIOutlines == 0: 
+    tempDisplay = TrackedObjects
+    
 tempDisplay = tempDisplay.astype('uint8')*255
 Display = np.maximum(Stack,tempDisplay)
-Display = np.maximum(Display,ROIMaskOutlines)
 
-with napari.gui_qt():
-    viewer = napari.view_image(Display)
-    viewer = napari.view_image(ROIMaskLabel)
+# with napari.gui_qt():
+#     viewer = napari.view_image(Display)
+#     viewer = napari.view_image(ROIMaskValid)
+#     viewer = napari.view_image(ROIMaskLabel)
 
 #%% Save datas
 
+MergedProps.to_csv(RootPath+Filename[0:-4]+'_MergedProps.csv')
 io.imsave(RootPath+Filename[0:-4]+'_Gradient.tif', Gradient, check_contrast=True)
 io.imsave(RootPath+Filename[0:-4]+'_Display.tif', Display, check_contrast=True)
-
